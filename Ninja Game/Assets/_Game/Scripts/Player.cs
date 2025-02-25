ac5df649243319c0481f997c4d37f518e0d5f644
ADMIN-PC\Admin <lamtrinhh1201@gmail.com>
2025-02-25 18:33:21 +0700

    fix attack anim

--- Ninja Game/Assets/_Game/Scripts/Player.cs
+++ Ninja Game/Assets/_Game/Scripts/Player.cs
@@ -27,11 +27,17 @@ namespace _Game.Scripts
 
         private void Update()
         {
+            
             if (!_isGrounded) return;   
             //jump
             if (Input.GetKeyDown(KeyCode.Space))
             {                
                 _isJumping = true;
+            }
+            //attack
+            if (Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.J))
+            {
+                _isAttacking = true;
             }
         }
 
@@ -42,9 +48,7 @@ namespace _Game.Scripts
             //jump
             if (_isJumping)
             {                
-                ChangeAnim("jump");
-                rb.AddForce(jumpForce * Vector2.up);
-                _isJumping = false;
+                Jump();
             }
             
             if (!_isGrounded && rb.velocity.y < 0)
@@ -57,14 +61,20 @@ namespace _Game.Scripts
 
             if (_isGrounded)
             {
-                // -1 -> 0 -> 1 
-                //run
-                if (Mathf.Abs(_horizontal) > 0.1f)
+                //change anim run
+                if (Mathf.Abs(_horizontal) > 0.1f && !_isAttacking)
                 {
                     ChangeAnim("run");
-                    /*rb.velocity = new Vector2(_horizontal * Time.fixedDeltaTime * speed, rb.velocity.y);
-                    transform.rotation = Quaternion.Euler(new Vector3(0, _horizontal > 0 ? 0 : 180, 0));*/
                 }
+                
+                //attack
+                if (_isAttacking)
+                {
+                    Attack();
+                }
+                /*//throw
+                Throw();*/
+                
             }
             
             //Moving
@@ -95,31 +105,39 @@ namespace _Game.Scripts
             return hit.collider != null;
         }
 
+        // ReSharper disable Unity.PerformanceAnalysis
         private void Attack()
         {
-        
+            ChangeAnim("attack");
+            //reset attack
+            float attackAnimLength = animator.GetCurrentAnimatorStateInfo(0).length;
+            Invoke(nameof(ResetAttack), 0.01f);
+        }
+          
+        private void ResetAttack()
+        {
+            _isAttacking = false;
+            ChangeAnim("idle");
         }
 
         private void Throw()
         {
-        
+            ChangeAnim("throw");
         }
 
         private void Jump()
         {
-        
+            ChangeAnim("jump");
+            rb.AddForce(jumpForce * Vector2.up);
+            _isJumping = false;
         }
 
         private void ChangeAnim(string animName)
         {
-            if (_currentAnimName != animName)
-            {
-                /*
-                animator.ResetTrigger(animName);
-                */
-                _currentAnimName = animName;
-                animator.SetTrigger(_currentAnimName);
-            }
+            if (_currentAnimName == animName) return;
+            animator.ResetTrigger(animName);
+            _currentAnimName = animName;
+            animator.SetTrigger(_currentAnimName);
         }
     }
 }
