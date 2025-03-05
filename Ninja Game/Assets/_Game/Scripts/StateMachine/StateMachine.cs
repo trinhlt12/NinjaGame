4da6bd167e9405c52422a4c8103498037cda6cdb
lamtrinhh1201 <lamtrinhh1201@gmail.com>
2025-03-05 14:18:07 +0700

    Add previous state & RevertToPreviousState function to SM
    Allow transition to previous state in some cases

--- Ninja Game/Assets/_Game/Scripts/StateMachine/StateMachine.cs
+++ Ninja Game/Assets/_Game/Scripts/StateMachine/StateMachine.cs
@@ -6,22 +6,47 @@ namespace _Game.Scripts.StateMachine
     public class StateMachine<T>  where T : Blackboard
     {
         public BaseState<T> CurrentState { get; set; }
+        public BaseState<T> PreviousState { get; set; }
 
         public void InitializeStateMachine(BaseState<T> initialState, T blackboard)
         {
             if (initialState == null) throw new ArgumentNullException(nameof(initialState));
             if (blackboard == null) throw new ArgumentNullException(nameof(blackboard));
 
-            CurrentState = initialState;
             blackboard.InitializeStates(this);
+            CurrentState = initialState;
+            PreviousState = null;
             CurrentState.Enter();
         }
         
         public void ChangeState(BaseState<T> newState)
         {
-            CurrentState?.Exit();
+            if (newState == null)
+            {
+                Debug.LogError("New state is null");
+                return;
+            };
+
+            if (CurrentState != null)
+            {
+                PreviousState = CurrentState;
+                CurrentState.Exit();
+            }
+            
             CurrentState = newState;
             CurrentState.Enter();
+        }
+
+        public void RevertToPreviousState()
+        {
+            if(PreviousState != null)
+            {
+                ChangeState(PreviousState);
+            }
+            else
+            {
+                Debug.LogWarning("No previous state");
+            }
         }
     }
 }
